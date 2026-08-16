0fef952f713c8007b6706fa30c480b9a9059564c
Md Adib Azam <mdadibazam8@gmail.com>
2026-08-16 15:28:49 +0530

    Apply V6 response guards to terminal runtime

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from jarvis.logging_utils import install_exception_hook
+from jarvis.runtime_guard import install_runtime_guards
 from jarvis.ui import run_cli
 
 
 if __name__ == '__main__':
     install_exception_hook()
+    install_runtime_guards()
     run_cli()
