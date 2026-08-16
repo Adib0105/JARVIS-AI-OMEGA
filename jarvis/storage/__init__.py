e3f6244e7140b977f5653acd403055f5d83c70c9
Md Adib Azam <mdadibazam8@gmail.com>
2026-08-16 18:13:57 +0530

    Export V7.5 backup and restore manager

--- jarvis/storage/__init__.py
+++ jarvis/storage/__init__.py
@@ -1,3 +1,4 @@
+from .backup import BackupManager, BackupManifest
 from .migrations import SchemaMigrator, TARGET_SCHEMA_VERSION
 
-__all__ = ['SchemaMigrator', 'TARGET_SCHEMA_VERSION']
+__all__ = ['BackupManager', 'BackupManifest', 'SchemaMigrator', 'TARGET_SCHEMA_VERSION']
