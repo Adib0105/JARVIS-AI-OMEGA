9f3b7f08d9a979d2798fdb17d46ed594a41199cd
Md Adib Azam <mdadibazam8@gmail.com>
2026-08-16 18:00:40 +0530

    Add V7.5 skill proposal and workflow learning package

---
+++ jarvis/skills/__init__.py
@@ -0,0 +1,7 @@
+from .manager import SkillManifest, SkillProposal, SkillProposalStatus, SkillRegistry
+from .workflow import RepeatedWorkflow, WorkflowLearningEngine
+
+__all__ = [
+    'SkillManifest', 'SkillProposal', 'SkillProposalStatus', 'SkillRegistry',
+    'RepeatedWorkflow', 'WorkflowLearningEngine',
+]
