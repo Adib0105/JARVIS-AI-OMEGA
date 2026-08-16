0affc36eb86dd5c54fb94fa1deaf3927f6e986a3
Md Adib Azam <mdadibazam8@gmail.com>
2026-08-16 15:28:43 +0530

    Launch V6 with response guards and adaptive ARC layout

--- desktop_app.py
+++ desktop_app.py
@@ -1,7 +1,8 @@
-from jarvis.gui import run_gui
 from jarvis.logging_utils import install_exception_hook
+from jarvis.runtime_guard import install_runtime_guards, run_adaptive_gui
 
 
 if __name__ == '__main__':
     install_exception_hook()
-    run_gui()
+    install_runtime_guards()
+    run_adaptive_gui()
