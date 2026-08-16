8bd5c6dd1f97db4f9b1164868b8289f185ae2398
Md Adib Azam <mdadibazam8@gmail.com>
2026-08-16 17:41:58 +0530

    Export capability gap detection API

--- jarvis/evaluation/__init__.py
+++ jarvis/evaluation/__init__.py
@@ -1,3 +1,7 @@
 from .engine import EvaluationMetric, EvaluationSnapshot, SelfEvaluationEngine
+from .gaps import CapabilityGap, CapabilityGapDetector, GapSeverity
 
-__all__ = ['EvaluationMetric', 'EvaluationSnapshot', 'SelfEvaluationEngine']
+__all__ = [
+    'EvaluationMetric', 'EvaluationSnapshot', 'SelfEvaluationEngine',
+    'CapabilityGap', 'CapabilityGapDetector', 'GapSeverity',
+]
