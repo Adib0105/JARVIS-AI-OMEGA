8bdf4145ffc2e9ea2112a8f963e09c738fb8f7f5
Md Adib Azam <mdadibazam8@gmail.com>
2026-08-16 17:02:54 +0530

    Install V7 voice player controls at desktop startup

--- desktop_app.py
+++ desktop_app.py
@@ -1,8 +1,10 @@
 from jarvis.logging_utils import install_exception_hook
 from jarvis.runtime_guard import install_runtime_guards, run_adaptive_gui
+from jarvis.voice_ui import install_voice_ui
 
 
 if __name__ == '__main__':
     install_exception_hook()
     install_runtime_guards()
+    install_voice_ui()
     run_adaptive_gui()
