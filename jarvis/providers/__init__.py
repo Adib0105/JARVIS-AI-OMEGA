735c4199c1489cbc7d5e94eb8d18c8ebfc56135b
Md Adib Azam <mdadibazam8@gmail.com>
2026-08-27 22:26:55 +0530

    feat: export provider runtime resilience services

--- jarvis/providers/__init__.py
+++ jarvis/providers/__init__.py
@@ -1,5 +1,11 @@
 from .base import AIProvider, ProviderTurn, ToolCall, ToolResult
 from .factory import create_local_provider, create_primary_provider
+from .registry import (
+    FallbackPolicy,
+    ProviderHealth,
+    ProviderHealthState,
+    ProviderRegistry,
+)
 
 __all__ = [
     'AIProvider',
@@ -8,4 +14,8 @@ __all__ = [
     'ToolResult',
     'create_primary_provider',
     'create_local_provider',
+    'FallbackPolicy',
+    'ProviderHealth',
+    'ProviderHealthState',
+    'ProviderRegistry',
 ]
